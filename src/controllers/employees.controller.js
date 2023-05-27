de9de893b1477450c968435b3618a4580b29ba3c
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2023-05-27 14:35:01 -0300

    catch errors added

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,48 +1,71 @@
 import { pool } from '../db.js'
 
 export const getEmployees = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM employee')
-    res.json(rows)
+    try {
+        const [rows] = await pool.query('SELECT * FROM employee');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
 export const getEmployee = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id]);
+    try {
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id]);
 
-    if (rows.length <= 0) return res.status(404).json({ message: 'Employee not found' })
+        if (rows.length <= 0) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
 
-    res.json(rows[0])
+        res.json(rows[0]);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
 
 export const createEmployees = async (req, res) => {
-    const { name, salary } = req.body
-    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
-    res.send({
-        id: rows.insertId,
-        name,
-        salary
-    })
-
+    try {
+        const { name, salary } = req.body;
+        const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary]);
+        res.send({
+            id: rows.insertId,
+            name,
+            salary
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
 export const deleteEmployees = async (req, res) => {
-    const [result] = await pool.query('DELETE FROM employee WHERE id = ?' , [req.params.id])
+    try {
+        const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id]);
 
-    if(result.affectedRows <= 0) return res.status(404).json({ message: 'Employee not found' })
-        
+        if (result.affectedRows <= 0) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
     
-    res.status(204) //Salió todo bien pero no respondo al cliente
+        res.sendStatus(204); // Salió todo bien pero no respondo al cliente
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
 export const updateEmployees = async (req, res) => {
-    const {id} = req.params
-    const {name, salary} = req.body
+    try {
+        const { id } = req.params;
+        const { name, salary } = req.body;
 
-    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id])
+        const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id]);
 
-    if(result.affectedRows <= 0) return res.status(404).json({ message: 'Employee not found' })
+        if (result.affectedRows <= 0) {
+            return res.status(404).json({ message: 'Employee not found' });
+        }
 
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
-    res.json(rows[0])
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id]);
+        res.json(rows[0]);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
-
